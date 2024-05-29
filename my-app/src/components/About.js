ce53dda94b5c501b229b42c5b92ee4649f5aa2fb
06yashi <137177587+06yashi@users.noreply.github.com>
2024-05-29 13:21:19 +0530

    Update About.js

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,12 +1,21 @@
+// src/components/About.js
 import React from 'react';
+import './About.css';  // Import the CSS file for additional About component styling
 
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <p>This is the About Page.</p>
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our company! We are committed to providing the best service in the industry.
+      </p>
+      <p>
+        Our team of experts is dedicated to ensuring your satisfaction. We believe in the power of
+        collaboration and innovation to drive progress and achieve great results.
+      </p>
     </div>
   );
 }
 
 export default About;
+
